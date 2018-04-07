a9104a09ac3255b0f2b9d83b5c174524998deaf1
ravindragsingh <arien.641@gmail.com>
2018-04-07 10:18:43 -0400

    Cleanup

--- drug-and-food-interaction-app/src/components/SubmitButton.js
+++ drug-and-food-interaction-app/src/components/SubmitButton.js
@@ -5,10 +5,10 @@ import { Button } from 'react-bootstrap';
 export default function SubmitButton (props) {
     return (
         <div>
-            // display on last page
+
             <Button color="success">Finish</Button>
 
-            //display on every page except the last page
+
             <Button color="primary">Next</Button>
 
         </div>
